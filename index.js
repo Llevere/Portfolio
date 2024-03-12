564ade34e3a19a7004b0a22fee64f20108fc9ab5
Llevere <mattlevere@hotmail.com>
2024-03-12 01:59:10 -0400

    Hopefully this fixes the responsiveness issue with the input sliders

--- index.js
+++ index.js
@@ -36,6 +36,24 @@ window.addEventListener(
   },
   false
 );
+function toggleDropdown() {
+  var dropdownContent = document.getElementById("dropdownContent");
+  dropdownContent.classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropdown-content")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
 
 // Get the range input element
 const rangeInput = document.getElementById("rangeInput");
